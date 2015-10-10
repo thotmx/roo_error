4c0c4741917f020a79f43dcdeaefb7c79bc4834b
Hermes Ojeda Ruiz <hermes.o.r@gmail.com>
2015-10-10 02:50:40 -0500

    Test with the master code from github and little changes
       * Show the 3 results with one execution
       * Only show the first 2 results for each file
       * Test directly with the master branch from github
       * Add a Gemfile to allow to change dependencies easier

---
+++ README.md
@@ -0,0 +1,8 @@
+# Roo - Error
+
+  * ```
+      bundle install
+    ```
+  * ```
+      bundle exec ruby test.rb
+    ```
--- test.rb
+++ test.rb
@@ -1,24 +1,27 @@
 require 'roo'
-file = File.open("#{File.dirname(__FILE__)}/example.xlsx")
-# file = File.open("#{File.dirname(__FILE__)}/example.ods")
-# file = File.open("#{File.dirname(__FILE__)}/example.csv")
+%w{ example.xlsx example.ods example.csv }.each do |file_name|
+  file = File.open(File.join(File.dirname(__FILE__), file_name))
 
-spreadsheet = case File.extname(file.path)
-	when ".csv" then Roo::CSV.new(file.path, csv_options: {col_sep: ";"})
-	when ".xls"  then Roo::Excel.new(file.path)
-	when ".xlsx" then Roo::Excelx.new(file.path)
-	when ".ods"  then Roo::OpenOffice.new(file.path)
-	else raise "Unknown file type: #{file.original_filename}"
+  spreadsheet = case File.extname(file.path)
+                when ".csv" then Roo::CSV.new(file.path, csv_options: {col_sep: ";"})
+                when ".xls"  then Roo::Excel.new(file.path)
+                when ".xlsx" then Roo::Excelx.new(file.path)
+                when ".ods"  then Roo::OpenOffice.new(file.path)
+                else raise "Unknown file type: #{file.original_filename}"
+                end
+
+  header = spreadsheet.row(1)
+
+  puts "*"*50
+  puts file_name
+  puts "*"*50
+
+  (2..3).map do |i|
+    row = Hash[[header, spreadsheet.row(i)].transpose]
+
+    puts row
+  end
 end
-
-header = spreadsheet.row(1)
-
-(2..spreadsheet.last_row).map do |i|
-  row = Hash[[header, spreadsheet.row(i)].transpose]
-
-  puts row
-end
-
 
 ########################################################
 #
